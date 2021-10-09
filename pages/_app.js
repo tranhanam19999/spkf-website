221a6f060d1c359ae63c3b546c6330c0ee28a845
Tran Ha Nam <tranhanam1999hn@gmail.com>
2021-10-09 13:47:08 +0700

    feat: socket chat

--- pages/_app.js
+++ pages/_app.js
@@ -1,30 +1,29 @@
-import React from "react";
-import NextApp from "next/app";
-import withReduxStore from '../lib/with-redux-store'
-import { Provider } from 'react-redux'
-
+import React from 'react';
+import NextApp from 'next/app';
+import withReduxStore from '../lib/with-redux-store';
+import { Provider } from 'react-redux';
 class App extends NextApp {
-  static async getInitialProps({ Component, ctx }) {
-    let pageProps = {};
+    static async getInitialProps({ Component, ctx }) {
+        let pageProps = {};
 
-    if (Component.getInitialProps) {
-      pageProps = await Component.getInitialProps(ctx);
+        if (Component.getInitialProps) {
+            pageProps = await Component.getInitialProps(ctx);
+        }
+
+        return {
+            pageProps,
+        };
     }
 
-    return { 
-      pageProps, 
-    };
-  }
+    render() {
+        const { Component, pageProps, reduxStore } = this.props;
 
-  render() {
-    const { Component, pageProps, reduxStore } = this.props;
-
-    return (
-      <Provider store={reduxStore}>
-        <Component {...pageProps} />
-      </Provider>
-    );
-  }
+        return (
+            <Provider store={reduxStore}>
+                <Component {...pageProps} />
+            </Provider>
+        );
+    }
 }
 
-export default withReduxStore(App)
+export default withReduxStore(App);
--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,23 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import {increValue, decreValue} from '../store/count/countSlice'
+import { increValue, decreValue } from '../store/count/countSlice';
 
 const IndexPage = () => {
-  const dispatch = useDispatch()
-  const state = useSelector(state => state.count)
-  useEffect(() => {
-    //console.log(state)
-  }, [state])
-  return(
-    <div>
-      <h1>Simple implement Next.js With Redux Toolkit</h1>
-      <h2>Counted: {state}</h2>
-      <hr/>
-      <button onClick={() => dispatch(increValue())}>Increment</button>
-      &nbsp;
-      <button onClick={() => dispatch(decreValue())}>decrement</button>
-    </div>
-  )
-}
-export default IndexPage
+    const dispatch = useDispatch();
+    const state = useSelector((state) => state.count);
+    useEffect(() => {
+        //console.log(state)
+    }, [state]);
+    return (
+        <div>
+            <h1>Simple implement Next.js With Redux Toolkit</h1>
+            <h2>Counted: {state}</h2>
+            <hr />
+            <button onClick={() => dispatch(increValue())}>Increment</button>
+            &nbsp;
+            <button onClick={() => dispatch(decreValue())}>decrement</button>
+        </div>
+    );
+};
+
+export default IndexPage;
