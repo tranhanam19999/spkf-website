aff5f8e9b3be724923dd9726a9d16d896810c0d7
vudung2000 <dangvudung18110263@gmail.com>
2021-12-24 21:46:22 +0700

    imp: report api

--- api/report.js
+++ api/report.js
@@ -2,15 +2,15 @@ import axios from 'axios';
 
 const host = 'https://spkf-api.herokuapp.com/report';
 
-export const reportApi = (postId, commentId, authorId, reportedType, reportedReason, token) => {
-    const data =  {reportedType, reportedReason, authorId}
+export const reportApi = (postId, commentId, authorId, reportType, content, token) => {
+    const data =  {reportType , content, authorId}
     if(commentId) {
         data.commentId = commentId
     } else {
         data.postId = postId
     }
     return axios({
-        method: 'PUT',
+        method: 'POST',
         headers: { Authorization: `${token}` },
         data,
         url: `${host}`,
